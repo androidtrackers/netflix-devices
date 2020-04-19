2ad904251f430213252b022fd81dbaf41d29b770
yshalsager <ysh-alsager@hotmail.com>
2020-04-19 22:24:38 +0200

    Initial commit
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# Netflix supported devices
+
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.png?v=103)](https://github.com/ellerbrock/open-source-badges/)
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+
+A Python 3 script that scrapes Netflix supported devices.
+
+Devices list is available [here](https://github.com/androidtrackers/netflix-devices/blob/master/netflix_devices.json)
---
+++ scraper.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Netflix supported devices tracker"""
+import json
+from datetime import date
+from os import environ, system
+
+from bs4 import BeautifulSoup
+from requests import get
+
+GIT_OAUTH_TOKEN = environ['XFU']
+
+# Netflix page variable
+URL = "https://help.netflix.com/en/node/23939"
+HD_SELECTOR = "div.panels > div.tab-panel:nth-of-type(4) > div.c-wrapper > " \
+              "div.accordion > div.c-wrapper > div.tab > div.tab-content > " \
+              "div.c-wrapper > div > ul > li > p"
+HDR_SELECTOR = "div.panels > div.tab-panel:nth-of-type(5) > div.c-wrapper > " \
+               "div.accordion > div.c-wrapper > div.tab > div.tab-content > " \
+               "div.c-wrapper > div > ul > li > p"
+
+
+def fetch(url):
+    """Fetch page content from URL"""
+    return get(url)
+
+
+def parse(response):
+    """Parse netflix page response"""
+    page = BeautifulSoup(response, 'html.parser')
+    hd_data = page.select(HD_SELECTOR)
+    hdr_data = page.select(HD_SELECTOR)
+    return {'hd_devices': [i.text.strip() for i in hd_data], 'hdr_devices': [i.text.strip() for i in hdr_data]}
+
+
+def write_data(data):
+    """Write data to JSON file"""
+    with open("netflix_devices.json", "w") as out:
+        json.dump(data, out, indent=1, ensure_ascii=False)
+
+
+def git_commit_push():
+    """
+    git add - git commit - git push
+    """
+    system(f'git add README.md && git -c user.name='
+           f'user.name=CI -c "user.email='
+           f'example@example.com" commit -m "[skip ci] sync: {str(date.today())}"'
+           f' && git push -q https://{GIT_OAUTH_TOKEN}@github.com/'
+           f'androidtrackers/netflix-devices.git HEAD:master')
+
+
+def main():
+    """
+    Netflix supported devices tracker
+    """
+    response = fetch(URL)
+    data = parse(response.text)
+    write_data(data)
+    git_commit_push()
+
+
+if __name__ == '__main__':
+    main()
